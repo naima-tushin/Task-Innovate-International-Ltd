fbf95358e35b021f3dfbba81eec1440121dbdbb3
naima-tushin <naima39tushin@gmail.com>
2024-09-21 04:38:46 +0600

    Added Explore Section

--- src/app/Components/HomePage/HomePage.jsx
+++ src/app/Components/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Signup from './Signup';
+import Explore from './Explore';
 
 const Homepage = () => {
     return (
         <div>
             <Banner />
             <Signup/>
+            <Explore/>
         </div>
     );
 };
--- src/app/Components/HomePage/Signup.jsx
+++ src/app/Components/HomePage/Signup.jsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 const Signup = () => {
     return (
-        <div className="flex items-center justify-center gap-40 h-screen my-52">
-            {/* Left Side */}
+        <div className="flex items-center justify-center gap-40 h-screen mt-72">
+            {/* Left Section */}
             <div className="relative flex flex-col items-center">
                 {/* Photo 1 */}
                 <div className="relative flex items-center px-8 w-64 h-[260px] rounded-[40px] bg-gradient-to-r from-[#1A0EA47D] to-[#1300EED4] text-white mr-96">
@@ -39,7 +39,6 @@ const Signup = () => {
                     </div>
                 </div>
 
-
                 {/* Photo 3 */}
                 <div className="relative flex items-center px-8 w-52 h-56 rounded-[40px] bg-gradient-to-r from-[#FA6F6F] to-[#A51D1D] text-white -mt-10 -ml-80">
                     <span className='text-4xl font-semibold'>Photo 3</span>
@@ -55,10 +54,10 @@ const Signup = () => {
                 </div>
             </div>
 
-            {/* Right Side */}
+            {/* Right Section */}
             <div className="max-w-lg space-y-12 mt-24">
                 <p className="text-2xl leading-relaxed text-[#000000] font-medium">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore e.
                 </p>
                 <div className="flex justify-center">
                     <button className="px-8 py-3 font-semibold border-[3px] rounded-2xl border-[#4000FF] text-[#4000FF] hover:bg-[#4000FF] hover:text-white">

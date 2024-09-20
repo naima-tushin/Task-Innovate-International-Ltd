07328079e01a6927ce9b2b8d1e2371ef8f77b2dd
naima-tushin <naima39tushin@gmail.com>
2024-09-20 23:27:47 +0600

    Added Navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
        <Navbar/>
-        <div className="h-screen">
+        <div>
         {children}
         </div>
         <footer>
